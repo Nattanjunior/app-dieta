c3be708f4c2d8f368558d3048113ca23344ce00c
nattanjunior <fnatanieljunior@gmail.com>
2024-09-18 18:44:48 -0300

    create: created instacie of class CreateNutritionController

--- backend/src/controllers/CreateNutritionController.ts
+++ backend/src/controllers/CreateNutritionController.ts
@@ -2,11 +2,28 @@ import {
   FastifyRequest,
   FastifyReply
 } from 'fastify'
+import { createNutritionService } from '../services/CreateNutritionServices'
+
+
+export interface Dataprops {
+  name: string,
+  weight: string,
+  height: string,
+  age: string,
+  gender: string,
+  objective: string,
+  level: string,
+}
 
 class CreateNutritioncontroller {
   async handle(req: FastifyRequest, res: FastifyReply) {
-    console.log("ROTA FOI CHAMADA!!")
-    res.send({ message: "ROTA FOI CHAMADA!!!" })
+    const { name, weight, height, age, objective, level, gender } = req.body as Dataprops
+
+    const create = new createNutritionService();
+    const nutrition = await create.execute({
+      name, weight, height, age, objective, level, gender
+    });
+    res.send(nutrition);
   }
 }
 
